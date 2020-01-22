41973e05f735bbd9081803d444980d8efe0c594d
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-22 11:39:09 -0300

    AsideBar exibido ao clicar no botao

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -3,10 +3,15 @@ import './style.css';
 import IconPlus from '../../assets/icons/plus.png';
 
 export default class Header extends Component {
+
+    showAsideBar() {
+        document.querySelector('nav.aside-bar').classList.toggle('hide-aside-bar');
+    }
+
     render() {
         return (
             <header className="header-main">
-                <button className="add-todo">
+                <button className="add-todo" onClick={this.showAsideBar}>
                     <p>Adicionar tarefa</p>
                     <img src={IconPlus} alt="Botão para adicionar tarefa" />
                 </button>
