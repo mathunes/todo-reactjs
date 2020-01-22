b167b33c18decff5b74bbbdf4378bde14d7e9152
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-22 11:27:51 -0300

    Adicao de icone do botao para adicionar tarefa

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
+import IconPlus from '../../assets/icons/plus.png';
 
 export default class Header extends Component {
     render() {
@@ -7,6 +8,7 @@ export default class Header extends Component {
             <header className="header-main">
                 <button className="add-todo">
                     <p>Adicionar tarefa</p>
+                    <img src={IconPlus} alt="Botão para adicionar tarefa" />
                 </button>
                 <h1>Tarefas</h1>
             </header>
