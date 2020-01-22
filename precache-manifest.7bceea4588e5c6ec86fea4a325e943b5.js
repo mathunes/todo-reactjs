98405b12eb3b284f07bbfeaf6e4a54161dd27928
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-22 20:13:08 -0300

    Updates

---
+++ precache-manifest.7bceea4588e5c6ec86fea4a325e943b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342a82e7628ef787e8dd65234d299865",
+    "url": "/todo-reactjs/index.html"
+  },
+  {
+    "revision": "d3cc2fed66c7c45d9937",
+    "url": "/todo-reactjs/static/css/main.5a8d66f2.chunk.css"
+  },
+  {
+    "revision": "e4cef2ca60367676514b",
+    "url": "/todo-reactjs/static/js/2.5ac9aa93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-reactjs/static/js/2.5ac9aa93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3cc2fed66c7c45d9937",
+    "url": "/todo-reactjs/static/js/main.522c8736.chunk.js"
+  },
+  {
+    "revision": "496f11d802a7d3013fc1",
+    "url": "/todo-reactjs/static/js/runtime-main.8b4fd274.js"
+  }
+]);
