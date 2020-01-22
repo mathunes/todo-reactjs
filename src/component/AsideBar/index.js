8b63bc213471c02c7ba44a4ba1f667c294946973
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-22 09:40:55 -0300

    Adicao do botao para fechar asidebar

--- src/component/AsideBar/index.js
+++ src/component/AsideBar/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './style.css';
+import IconBack from '../../assets/icons/back.png';
 
 export default class AsideBar extends Component {
     render() {
         return (
             <nav className="aside-bar">
-                <h1>Adicionar tarefa</h1>
+                <header>
+                    <img src={IconBack} alt="Botão para fechar asidebar" />
+                    <h1>Adicionar tarefa</h1>
+                </header>
                 <form>
                     <input type="text" className="title-todo" placeholder="Título"/>
                     <textarea type="text" className="description-todo" placeholder="Descrição"/>
